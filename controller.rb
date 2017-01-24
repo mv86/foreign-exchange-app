788dbcde2a69bcc4d4248f30f18fd2690de43d41
Max <mveasey87@gmail.com>
2017-01-24 14:53:44 +0000

    added select_populator helper and edited views.erb

--- controller.rb
+++ controller.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'sinatra/contrib/all'
 require_relative './models/exchange_rate'
+require_relative './helpers/select_populator.rb'
 
 get '/' do
+  @currencies = Select_populator
   erb(:home)
 end
